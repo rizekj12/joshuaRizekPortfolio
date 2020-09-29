40b2304e9cfc510a28e86dd05f43d91ac0360804
Joshua Rizek <Joshua Rizek>
2020-09-29 10:40:45 -0400

    changed about me

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -6,7 +6,7 @@ export default function AboutMe() {
         <section className="aboutMeSect" id="about-me">
             <div className="aboutMeDiv">
                 <h1 className="aboutMeHeader">About me!</h1>
-                <p className="aboutMePara">I am software developer who loves to constantly learn and challenge myself through problem-solving. My undergraduate studies in philosophy have helped me to become a strong developer by thinking outside the box when problem solving and being capable of conceptually understanding new technical frameworks.  My passion projects consist of apps that improve mental health and developing games. On my free time I enjoy competitive gaming, mixed martial arts, and traveling! I am someone who enjoys working in diverse groups because I believe we can call learn more from each other as developers as well as people. Check out the rest of my site to see some of my work and reach out to me to work together! </p>
+                <p className="aboutMePara">I am software developer who loves to constantly learn and challenge myself through problem-solving. My undergraduate studies in philosophy have helped me to become a strong developer by thinking outside the box when problem solving and being capable of conceptually understanding new technical frameworks. My passion projects consist of apps that improve mental health and developing games. In my free time I enjoy competitive gaming, mixed martial arts, and traveling! I am someone who enjoys working in diverse groups because I believe we can call learn more from each other as developers as well as people. Check out the rest of my site to see some of my work and reach out to me to work together! </p>
             
             <div className="resourceDiv">
                 <a 
